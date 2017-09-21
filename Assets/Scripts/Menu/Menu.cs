6025a369bb902ddef6b972e933791d7fa37fec40
Wilkinson D. de Souza <wilkinson@outlook.com.br>
2017-09-20 22:40:57 -0300

    Transition from menu to main scene

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : SingletonObject {
+    private Fading fadeControl;
+
+    private void Start()
+    {
+        fadeControl = this.GetComponent<Fading>();
+    }
+
+    void Update () {
+        if (Input.GetKey(KeyCode.Space)){
+            StartCoroutine(ChangeLevel("Main"));            
+        }		
+	}
+
+    IEnumerator ChangeLevel(string SceneName)
+    {
+        fadeControl.BeginFade(1);
+        yield return new WaitForSeconds(fadeControl.fadeSpeed);
+        Scenes.LoadScene("Main");
+    }
+}
---
+++ Assets/Scripts/Utils/Scenes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour {
+
+    public static void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+    }
+}
