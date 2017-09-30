5af14183d5dd4cc025b4dc167520d3d5fa2f9bce
Wilkinson D. de Souza <wilkinson@outlook.com.br>
2017-09-30 17:02:50 -0300

    Cutscene control

---
+++ Assets/Scripts/Cutscene/Cutscene.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class CutsceneScene
+{
+    public AudioClip Voice;
+    public Sprite CutsceneImage; 
+    public float Duration = 1.0f;
+}
+
+public class Cutscene : MonoBehaviour
+{
+    private byte CurrentScene;
+    private AudioSource audioControl;
+    private Image imageRenderer;
+
+    public CutsceneScene[] Config;
+    public Object SceneToLoad;
+
+	void Start ()
+    {
+        audioControl = GetComponent<AudioSource>();
+        imageRenderer = GetComponent<Image>();
+        ChangeScene();
+    }
+
+    void ChangeScene()
+    {
+        CutsceneScene sceneToPlay = Config[CurrentScene];
+
+        //audioControl.Stop();
+        //audioControl.clip = sceneToPlay.Voice;
+
+        imageRenderer.sprite = sceneToPlay.CutsceneImage;
+        CurrentScene++;
+        if (CurrentScene >= Config.Length)
+            Invoke("ChangeToNextScene", sceneToPlay.Duration); 
+        else
+            Invoke("ChangeScene", sceneToPlay.Duration);
+
+    }
+
+    void ChangeToNextScene()
+    {
+        Scenes.LoadScene(SceneToLoad.name);
+    }
+}
